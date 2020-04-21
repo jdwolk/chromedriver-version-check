9465f5a53196c68f3644071b17ea8af01484ca46
JD Wolk <jd@philosophie.is>
2020-04-21 11:00:43 -0700

    Adds conditional tracking

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import mixpanel from 'mixpanel-browser';
+import { MixpanelProvider, MixpanelConsumer } from 'react-mixpanel';
 import * as serviceWorker from './serviceWorker';
+
+const MIXPANEL_TOKEN = process.env.REACT_APP_MIXPANEL_TOKEN;
+if (MIXPANEL_TOKEN) { mixpanel.init(MIXPANEL_TOKEN); }
+
+const MixpanelBS = ({ children }) => {
+  if (!MIXPANEL_TOKEN) { return children; }
+
+  const trackPageView = (mixpanel) => {
+    mixpanel.track('Page loaded');
+  };
+
+  return (
+    <MixpanelProvider mixpanel={mixpanel}>
+      <>
+        <MixpanelConsumer>
+          { trackPageView }
+        </MixpanelConsumer>
+        {children}
+      </>
+    </MixpanelProvider>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MixpanelBS>
+      <App />
+    </MixpanelBS>
   </React.StrictMode>,
   document.getElementById('root')
 );
